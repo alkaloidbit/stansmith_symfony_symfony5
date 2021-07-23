a5c58aa2e9ce34e61953e449f21d6d3200fb0199
fredbadlieutenant <fredoudjoudi@gmail.com>
2021-07-23 19:40:13 +0200

    add component for track list on playlist

---
+++ assets/js/vue/components/track-list-playlist/track-card.vue
@@ -0,0 +1,61 @@
+<template>
+    <div
+        class="trackCardRootContainer on-playlist playlist-item-renderer"
+        :class="[{'current-track': track === currentTrack && isPlaying,
+                  'is-loading': track === currentTrack && isLoading,
+                  'is-playing': track === currentTrack && isPlaying}]"
+        @click.prevent="handleClick(track)"
+    >
+        <div class="left-item">
+            <span
+                class="tracknumber"
+            >{{ track.tracknumber }}</span>
+            <span
+                v-if="!isLoading "
+                class="play icon is-medium"
+            >
+                <i class="mdi mdi-24px mdi-play" />
+            </span>
+            <img
+                :src="track.thumbnail.contentUrl"
+                alt=""
+            >
+            <span class="sound-bars" />
+        </div>
+        <div class="middle-item title-wrapper">
+            <span>{{ track.title }}</span>
+            <span>{{ track.artist.name }}</span>
+        </div>
+        <div
+            class="middle-item title-wrapper"
+        >
+            {{ track.album }}
+        </div>
+        <div class="duration right-item">
+            {{ track.playtime_string }}
+        </div>
+    </div>
+</template>
+
+<script>
+
+import playerMixin from '@/mixins/playerMixin';
+
+export default {
+    name: 'TrackCard',
+    mixins: [playerMixin],
+    props: {
+        track: {
+            type: Object,
+            required: true,
+        },
+    },
+    created() {
+    },
+    methods: {
+        handleClick(track) {
+            this.$emit('clicked', track);
+        },
+    },
+};
+</script>
