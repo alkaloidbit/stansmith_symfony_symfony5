4ff0fbd2c6e9614f875d18dc425fd9f25f92e369
fredbadlieutenant <fredoudjoudi@gmail.com>
2022-01-24 22:50:14 +0100

    Add CoverField and ThumbnailField components

---
+++ my-admin/src/ThumbnailField.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { useRecordContext } from 'react-admin';
+
+const ThumbnailField = ({ source }) => {
+    const record = useRecordContext();
+    const res = record['thumbnails'][0]['contentUrl'];
+    return record ? (
+        <img src={"http://localhost:8000"+ res} alt="" />
+    ) : null;
+}
+
+export default ThumbnailField;
