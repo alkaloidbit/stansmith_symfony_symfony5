a1d25ee2e82bd4f34919e8d53b85f67ee85e184c
fredbadlieutenant <fredoudjoudi@gmail.com>
2022-02-01 19:09:26 +0100

    delete unwanted property in VichSubscriber

--- src/EventSubscriber/VichSubscriber.php
+++ src/EventSubscriber/VichSubscriber.php
@@ -4,9 +4,7 @@ namespace App\EventSubscriber;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use App\Service\Configuration;
 use Vich\UploaderBundle\Event\Event;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Liip\ImagineBundle\Service\FilterService;
 use Liip\ImagineBundle\Factory\Config\FilterFactoryInterface;
 use Liip\ImagineBundle\Controller\ImagineController;
@@ -15,29 +13,26 @@ use Liip\ImagineBundle\Controller\ImagineController;
 class VichSubscriber implements EventSubscriberInterface
 {
 
-    private $configuration;
-    private $container;
     private $filterService;
 
-    public function __construct(FilterService $filterService, Configuration $configuration, ContainerInterface $container)
+    public function __construct(FilterService $filterService)
     {
-        $this->configuration = $configuration;
-        $this->container = $container;
         $this->filterService = $filterService;
     }
 
     public static function getSubscribedEvents()
     {
         return array(
-           'vich_uploader.post_upload' => 'onVichUploaderPostUpload',
+           'vich_uploader.pre_upload' => 'onVichUploaderPreUpload',
         );
     }
 
-    public function onVichUploaderPostUpload(Event $event)
+    public function onVichUploaderPreUpload(Event $event)
     {
-        $imageName = $event->getObject()->getImageName();
+        /* dd($event->getObject()); */
+        $imageName = $event->getObject()->fileName;
         $path      = $event->getMapping()->getUploadDestination();
-        $resourcePath = $this->filterService->getUrlOfFilteredImage($path.$imageName, 'my_thumb');
+        $resourcePath = $this->filterService->getUrlOfFilteredImage('uploads/covers/thumbnails/'.$imageName, 'my_thumb');
 
         dd($resourcePath);
     }
