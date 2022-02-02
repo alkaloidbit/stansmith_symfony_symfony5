f90d3ebb24651d0d0f4a8dd00351dd5d840f0d7e
fredbadlieutenant <fredoudjoudi@gmail.com>
2022-02-02 01:31:06 +0100

    Make thumbnail file on VichUpload Pre Upload event

--- src/EventSubscriber/VichSubscriber.php
+++ src/EventSubscriber/VichSubscriber.php
@@ -1,6 +1,10 @@
 <?php
 namespace App\EventSubscriber;
 
+use App\Entity\MediaObject;
+use Liip\ImagineBundle\Imagine\Cache\CacheManager;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -8,32 +12,73 @@ use Vich\UploaderBundle\Event\Event;
 use Liip\ImagineBundle\Service\FilterService;
 use Liip\ImagineBundle\Factory\Config\FilterFactoryInterface;
 use Liip\ImagineBundle\Controller\ImagineController;
+use Intervention\Image\Constraint;
+use Intervention\Image\ImageManager;
 
 
 class VichSubscriber implements EventSubscriberInterface
 {
 
+    private const DEFAULT_QUALITY = 80;
+
     private $filterService;
 
-    public function __construct(FilterService $filterService)
+    private $cacheManager;
+
+    private $imageManager;
+
+    public function __construct(ImageManager $imageManager, FilterService $filterService, CacheManager $imagineCacheManager)
     {
         $this->filterService = $filterService;
+        $this->cacheManager = $imagineCacheManager;
+        $this->imageManager = $imageManager;
     }
 
     public static function getSubscribedEvents()
     {
         return array(
            'vich_uploader.pre_upload' => 'onVichUploaderPreUpload',
+           'vich_uploader.post_upload' => 'onVichUploaderPostUpload',
         );
     }
 
     public function onVichUploaderPreUpload(Event $event)
     {
-        dd($event->getObject());
-        $imageName = $event->getObject()->fileName;
-        $path      = $event->getMapping()->getUploadDestination();
-        $resourcePath = $this->filterService->getUrlOfFilteredImage('uploads/covers/thumbnails/'.$imageName, 'my_thumb');
+        if ($event->getMapping()->getFilePropertyName() === 'thumbnail') {
+            return;
+        }
 
-        dd($resourcePath);
+        $object = $event->getObject();
+        $file = $object->getFile();
+
+        // boysnoize_superacide.jpg
+        dump($file->getClientOriginalName());
+
+        $data = file_get_contents($file->getPathname());
+
+        
+        // writeBinaryData
+        $destination = 'uploads/covers/thumbnails/'.sprintf('%s_%s', uniqid(), $file->getClientOriginalName());
+        $img = $this->imageManager
+             ->make($data)
+             ->resize(
+                 60,
+                 null,
+                 static function (Constraint $constraint): void {
+                     $constraint->upsize();
+                     $constraint->aspectRatio();
+                 }
+        );
+
+        $img->save($destination, $config['quality'] ?? self::DEFAULT_QUALITY);
+    }
+
+    public function getAlbumCoverPath($name, $extension)
+    {
+        return $this->mediaPathCover.'/'.sprintf('%s.%s', $name, $extension);
+    }
+
+    public function onVichUploaderPostUpload(Event $event)
+    {
     }
 }
