dd29d93a89e9aba3e1f1b600db8905c0f9b06a54
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-02-18 22:44:44 +0100

    Add missing properties in MediaImageService

--- src/Service/MediaImageService.php
+++ src/Service/MediaImageService.php
@@ -11,6 +11,19 @@ use App\Service\ImageManagerService;
 
 class MediaImageService
 {
+    /**
+     * @var ImageManagerService
+     */
+    private $ImageManagerService;
+    /**
+     * @var string
+     */
+    private $mediaPathCover;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
     public function __construct(ImageManagerService $imageService, EntityManagerInterface $em, string $mediaPathCover)
     {
         $this->ImageManagerService = $imageService;
@@ -59,8 +72,8 @@ class MediaImageService
             
             // Associate album with mediaObject
             $album->addCover($mediaObject);
-        } catch (\Exception $e) {
             // handle log exception
+        } catch (\Exception $e) {
         }
     }
 
