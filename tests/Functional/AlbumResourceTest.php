75a19798338b76226b9f716b296e18b6dbea814f
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-02-23 17:26:29 +0100

    Init tests/Functional dir
    https://symfonycasts.com/screencast/api-platform-security/backport-api-tests

---
+++ tests/Functional/AlbumResourceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+
+class AlbumResourceTest extends CustomApiTestCase
+{
+    public function testCreateAlbum()
+    {
+        $this->assertEquals(42, 42);
+    }
+}
