2f8aecd3840da488145509c4e844e0fb4fbc06c6
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-02-23 21:45:05 +0100

    Test login return 204

--- tests/Functional/AlbumResourceTest.php
+++ tests/Functional/AlbumResourceTest.php
@@ -3,11 +3,60 @@
 namespace App\Tests\Functional;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\User;
 
-class AlbumResourceTest extends CustomApiTestCase
+class AlbumResourceTest extends ApiTestCase
 {
-    public function testCreateAlbum()
+    public function testLogin():void
     {
-        $this->assertEquals(42, 42);
+        $client = self::createClient();
+        $user = new User();
+        $user->setEmail('testalbum@example.com');
+        $user->setUsername('testalbum');
+        $user->setPassword('$argon2id$v=19$m=65536,t=4,p=1$RHBOcjNJRTZkdWh5TXlRTw$Gzsz02Ef3fducRPvQtx4s3qPrCAZSIJJTwGRLcTkhIU');
+
+        $em = self::$container->get('doctrine')->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        $client->request('POST', '/login', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => 'testalbum@example.com',
+                'password' => 'foo'
+            ],
+        ]);
+
+        $this->assertResponseStatusCodeSame(204);
+
     }
+
+    /* public function testCreateAlbum(): void
+    {
+        $client = self::createClient();
+        $client->request('POST', '/api/albums', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [],
+        ]);
+        $this->assertResponseStatusCodeSame(401);
+
+        $user = new User();
+        $user->setEmail('testalbum@example.com');
+        $user->setUsername('testalbum');
+        $user->setPassword('$argon2id$v=19$m=65536,t=4,p=1$RHBOcjNJRTZkdWh5TXlRTw$Gzsz02Ef3fducRPvQtx4s3qPrCAZSIJJTwGRLcTkhIU');
+
+        $em = self::$container->get('doctrine')->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        $client->request('POST', '/login', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => 'testalbum@example.com',
+                'password' => 'foo'
+            ],
+        ]);
+
+        $this->assertResponseStatusCodeSame(204);
+    } */
 }
