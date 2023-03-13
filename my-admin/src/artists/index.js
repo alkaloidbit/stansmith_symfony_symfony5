d42cdd6536219ecf893f48fee2dae5f43266c70b
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-03-13 02:21:48 +0100

    ignoring my-admin dir

--- my-admin/src/artists/index.js
+++
@@ -1,9 +0,0 @@
-import ArtistsList from './ArtistsList';
-import ArtistEdit from './ArtistEdit';
-
-const ArtistComponents = {
-    list: ArtistsList,
-    edit: ArtistEdit,
-};
-
-export default ArtistComponents;
--- my-admin/src/tracks/index.js
+++
@@ -1,7 +0,0 @@
-import TracksList from './Tracks';
-
-const TrackComponents = {
-    list: TracksList,
-}
-
-export default TrackComponents;
