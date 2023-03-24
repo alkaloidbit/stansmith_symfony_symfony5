b94d4259db7043a14b0e0adf9c737c6b8fb39855
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-03-24 21:50:37 +0100

    AuthProvider: Convert to async methods

--- assets/react/admin/authProvider.js
+++ assets/react/admin/authProvider.js
@@ -1,44 +1,40 @@
 // in src/authProvider.js
 const authProvider = {
-    login: ({ email, password }) =>  {
+    login: async ({ email, password }) =>  {
         const request = new Request('/api/security/login', {
             method: 'POST',
             body: JSON.stringify({ email, password }),
             credentials: 'include',
             headers: new Headers({ 'Content-Type': 'application/json' })
         });
-        return fetch(request)
-            .then(response => {
-                if (response.status < 200 || response.status >= 300) {
-                    throw new Error(response.statusText);
-                }
-                localStorage.setItem('userIri', response.headers.get('Location'));
-                return Promise.resolve();
-            })
-            .catch(() => {
-                throw new Error('Network error')
-            });
+        try {
+            const response = await fetch(request);
+            if (response.status < 200 || response.status >= 300) {
+                throw new Error(response.statusText);
+            }
+            localStorage.setItem('userIri', response.headers.get('Location'));
+            return await Promise.resolve();
+        } catch {
+            throw new Error('Network error');
+        }
     },
     checkAuth: () => {
         // Required for the authentication to work
         return Promise.resolve();
     },
-    getIdentity: () => {
+    getIdentity: async () => {
         try {
             const userIri = localStorage.getItem('userIri');
-            return fetch(userIri)
-            .then(response => {
-                return response.json();
-            })
-            .then(json => {
+            try {
+                const response = await fetch(userIri);
+                const json = await response.json();
                 const { username } = json;
                 const fullName = username;
                 const id = json['@id'];
-                return Promise.resolve({ fullName, id });
-            })
-            .catch(()=> {
+                return await Promise.resolve({ fullName, id });
+            } catch {
                 throw new Error('Network error');
-            });
+            }
         } catch (error) {
             return Promise.reject(error);
         }
