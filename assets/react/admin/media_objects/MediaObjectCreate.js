98395ef9e0949e44521580b969426fc3b5edd3a5
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-04-10 00:57:40 +0200

    feat:Redirecting to the cover tab of the album

--- assets/react/admin/media_objects/MediaObjectCreate.js
+++ assets/react/admin/media_objects/MediaObjectCreate.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocation } from 'react-router';
 import {
     Create,
     SimpleForm,
@@ -7,9 +8,22 @@ import {
 } from 'react-admin';
 
 const MediaObjectCreate = props => {
+    const location = useLocation();
+
+    const album_id =
+        location.state && location.state.record
+            ? location.state.record.album_id
+            : undefined;
+
+    const redirect  = album_id ? `/albums/${album_id}/show/media_objects` : false;
+    console.log(redirect);
+
     return (
         <Create {...props}>
-            <SimpleForm addlabel="false">
+            <SimpleForm
+                addlabel="false"
+                redirect={redirect}
+            >
                 <ImageInput source="file" accept="image/*" >
                     <ImageField source="src" title="" />
                 </ImageInput>
