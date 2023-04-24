6b3aaa0430915e497081f07242204e111e7b1e8b
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-04-24 21:49:15 +0200

    User Test

--- tests/Functional/UserResourceTest.php
+++ tests/Functional/UserResourceTest.php
@@ -3,47 +3,29 @@
 namespace App\Tests\Functional;
 
 use Zenstruck\Foundry\Test\ResetDatabase;
-use App\Test\CustomApiTestCase;
 
-class UserResourceTest extends CustomApiTestCase
+class UserResourceTest extends ApiTestCase
 {
     use ResetDatabase;
 
-    public function testCreateUser(): void
+    public function testPostToCreateUser(): void
     {
-        $client = self::createClient();
-
-        $client->request('POST', '/api/users', [
-            'json' => [
-                'email' => 'testuserresource@example.com',
-                'username' => 'testuserresource',
-                'password' => 'foo'
-            ],
-        ]);
-
-        $this->assertResponseStatusCodeSame(201);
-
-        // use base class shortcut method to logIn
-        $this->logIn($client, 'testuserresource@example.com', 'foo');
-    }
-
-    public function testUpdateUser(): void
-    {
-        $client = self::createClient();
-        // use base class shortcut method 
-        $user = $this->createUserAndLogIn($client, 'testuserresource@example.com', 'foo');
-
-        $client->request('PUT', '/api/users/'.$user->getId(), [
-            'json' => [
-                'username' => 'newusername'
-            ]
-        ]);
-
-        $this->assertResponseIsSuccessful();
-        $this->assertJsonContains([
-            'username' => 'newusername'
-        ]);
+        $this->browser()
+            ->post('/api/users', [
+                'json' => [
+                    'email' => 'draggin_in_the_morning@coffee.com',
+                    'username' => 'draggin_in_the_morning',
+                    'password' => 'password',
+                ]
+            ])
+            ->assertStatus(201)
+            ->post('/api/security/login', [
+                'json' => [
+                    'email' => 'draggin_in_the_morning@coffee.com',
+                    'password' => 'password',
+                ]
+            ])
+            ->assertSuccessful()
+        ;
     }
 }
-
-
