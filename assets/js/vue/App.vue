83663baf1ab73dd7e82eeba147f493dcd5943e99
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-04-27 18:22:58 +0200

    fix: Prettier formatting

--- assets/js/vue/App.vue
+++ assets/js/vue/App.vue
@@ -2,20 +2,22 @@
     <router-view />
 </template>
 
-<script >
-import axios from 'axios';
+<script>
+import axios from "axios";
 
 export default {
-    name: 'App',
+    name: "App",
     created() {
         // Server response: isAuthenticated && User
-        const isAuthenticated = JSON.parse(this.$parent.$el.attributes['data-is-authenticated'].value);
+        const isAuthenticated = JSON.parse(
+            this.$parent.$el.attributes["data-is-authenticated"].value
+        );
         // User from server
-        const user = JSON.parse(this.$parent.$el.attributes['data-user'].value);
+        const user = JSON.parse(this.$parent.$el.attributes["data-user"].value);
 
         const payload = { isAuthenticated, user };
         //  Updating states
-        this.$store.dispatch('security/onRefresh', payload);
+        this.$store.dispatch("security/onRefresh", payload);
 
         axios.interceptors.request.use((req) => {
             console.log(`Request ${req.method} ${req.url}`);
@@ -24,13 +26,13 @@ export default {
 
         axios.interceptors.response.use(
             (res) => {
-                console.log('Response.data:', res.data);
+                console.log("Response.data:", res.data);
                 return res;
             },
             (err) => {
                 if (err.response.status === 401) {
-                    if (this.$route.path !== '/login') {
-                        this.$router.push({ path: '/login' });
+                    if (this.$route.path !== "/login") {
+                        this.$router.push({ path: "/login" });
                     }
                 } else if (err.response.status === 500) {
                     document.open();
@@ -38,7 +40,7 @@ export default {
                     document.close();
                 }
                 throw err;
-            },
+            }
         );
     },
 };
